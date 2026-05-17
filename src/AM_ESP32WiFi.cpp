0888ac676c621367e74e3f3a650ddd72ff193d05
Fabrizio Boco <fabrizio.boco@gmail.com>
2026-05-17 09:38:29 +0200

    Fixed ADC reading-to-voltage conversion formula

--- src/AM_ESP32WiFi.cpp
+++ src/AM_ESP32WiFi.cpp
@@ -370,7 +370,7 @@ void AMController::writeMessage(const char *variable, float value) {
   if (_pClient == NULL)
     return;
 
-  snprintf(buffer, VARIABLELEN + VALUELEN + 3, "%s=%.3f#", variable, value);
+  snprintf(buffer, VARIABLELEN + VALUELEN + 3, "%s=%.5f#", variable, value);
   _pClient->write((const uint8_t *)buffer, strlen(buffer)*sizeof(char));
 }
 
@@ -404,7 +404,7 @@ void AMController::temporaryDigitalWrite(uint8_t pin, uint8_t value, unsigned lo
 }
 
 float AMController::to_voltage(float adc_value, float vref, uint8_t resolution) {
-  const float conversion_factor = vref / (1 << resolution);
+  const float conversion_factor = vref / ((1 << resolution) - 1);
   return adc_value * conversion_factor;
 }
 
